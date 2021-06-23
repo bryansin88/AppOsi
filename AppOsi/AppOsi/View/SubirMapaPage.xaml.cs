980e791ad4eec106510c7784463ece15f989f99c
bryan peñaherrera <bryanpenaherrera88@gmail.com>
2021-06-23 18:19:35 -0500

    dsda

--- AppOsi/AppOsi/View/SubirMapaPage.xaml.cs
+++ AppOsi/AppOsi/View/SubirMapaPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Android;
-using Plugin.Geolocator;
+﻿using Plugin.Geolocator;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
@@ -10,6 +9,20 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppOsi.Model;
+
+using System.Net.Http;
+using Xamarin.Android;
+using Newtonsoft.Json;
+using System.IO;
+using Android.Graphics;
+using Java.IO;
+using System.Drawing;
+using Image = Xamarin.Forms.Image;
+
+using System.Net;
+using Android.Widget;
+using Bitmap = Android.Graphics.Bitmap;
 
 namespace AppOsi.View
 {
@@ -17,19 +30,24 @@ namespace AppOsi.View
     public partial class SubirMapaPage : ContentPage
     {
         double latitud; 
+         ImageView imagen2;
             double longi;
         private MediaFile _mediaFile;
 
-        
+        Bitmap bitmap;
         public SubirMapaPage()
         {
             InitializeComponent();
+            cargarMapas();
             btnSelec.Clicked += delegate
              {
                  Localizar();
              };
            
         }
+
+
+      
         public async Task<PermissionStatus> CheckAndRequestLocationPermission()
         {
             var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
@@ -110,12 +128,95 @@ namespace AppOsi.View
 
             
         }
-
-        private void Button_Clicked_1(object sender, EventArgs e)
+        public void cargarMapas()
         {
+            spnDesastre.Items.Add("Mapa 1");
+            spnDesastre.Items.Add("Mapa 2");
+            spnDesastre.Items.Add("Mapa 3");
+            spnDesastre.Items.Add("Mapa 4");
+            spnDesastre.Items.Add("Mapa 5");
+            spnDesastre.Items.Add("Mapa 6");
+            spnDesastre.Items.Add("Mapa 7");
+            spnDesastre.Items.Add("Mapa 8");
+            spnDesastre.Items.Add("Mapa 9");
+            spnDesastre.Items.Add("Mapa 10");
+        }
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            
+         
+
+            byte[] imageBytes;
+            var FileImage = new Image();
+            imageBytes = Convert.FromBase64String(logo.ToString());
+           
+            imagen2.SetImageBitmap(bitmap);
+            byte[] bitmapData;
+
+            using (var stream = new MemoryStream())
+           {
+
+               bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
+              bitmapData = stream.ToArray();
+
+
+           }
+           
+            var imagen = new ImagenS
+            {
+              
+                Imagen = filename.ToString(),
+              
+             
+            };
+            WebClient client = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("Nombre", "prueba");
+            parametros.Add("ImagenS", bitmapData.ToString());
+            parametros.Add("Rucempresa", "124");
+            parametros.Add("Mapa", spnDesastre.SelectedItem.ToString());
+
+
+
+            client.UploadValues("http://192.168.1.109/CenturiosWS/public/api/mapas/nuevo1", "POST", parametros);
+            await DisplayAlert("Confirmacion", "Mapa subido", "Aceptar");
+            return;
+            // byte[] bitmapData;
+
+            //using (var stream = new MemoryStream())
+            //{
+
+            //    bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
+            //    bitmapData = stream.ToArray();
+
+
+            //}
+
+
+            // await DisplayAlert("Confirmacion", "Mapa subido "+nombre.Text+"-"+_mediaFile +"-"+spnDesastre.SelectedItem, "Aceptar");
+
+            //var jsonRequest = JsonConvert.SerializeObject(FileImage);
+            //var content = new StringContent(jsonRequest, Encoding.UTF8, "TEXT/JSON");
+
+            //string result;
+            //try
+            //{
+            //    HttpClient client = new HttpClient();
+            //    client.BaseAddress = new Uri("http://192.168.1.109");
+            //    string url = string.Format("/CenturiosWS/public/api/mapa/nuevo1");
+            //    var response = await client.PostAsync(url, content);
+            //    result = response.Content.ReadAsStringAsync().Result;
+            //}
+            //catch (Exception)
+            //{
+            //    await DisplayAlert("Error", "No hay conexion, intente mas tarde", "Aceptar");
+            //    return;
+
+            //}
+            //await DisplayAlert("Confirmacion", "Mapa subido", "Aceptar");
 
         }
 
-      
+
     }
 }
---
+++ AppOsi/AppOsi/vista_comunicacion/alertaComunicacion.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppOsi.vista_comunicacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class alertaComunicacion : ContentPage
+    {
+        public alertaComunicacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
